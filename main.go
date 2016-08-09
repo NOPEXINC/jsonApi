660733e47ab089f46b9337e5cae9a953e810bc94
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-09 11:08:45 +0300

    Finish wiring up the main function

--- main.go
+++ main.go
@@ -1,2 +1,20 @@
 // returns a json from an external API
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	http.HandleFunc("/api" / PostsHandler)
+	log.Println("listening on http://localhost:" + port)
+	http.ListenAndServe(":"+port, nil)
+}
