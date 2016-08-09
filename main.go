ee4f558910575904b4eea4597ecc82c6b76c462a
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-09 11:18:50 +0300

    Add the api posts handle function

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"os"
 )
 
+type Post struct {
+	UserId int    `json:"userId"`
+	PostId int    `json::"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -17,4 +24,15 @@ func main() {
 	http.HandleFunc("/api" / PostsHandler)
 	log.Println("listening on http://localhost:" + port)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func PostsHandler(res http.ResponseWriter, req *http.Request) {
+	url := "https://jsonplaceholder.typicode.com/posts"
+	posts := make([]Post, 0)
+
+	GetJSON(url, &posts)
+
+	data := string(json.Marshal(posts))
+	log.Println("%s request from %s", req.Method, req.URL.Path)
+	fmt.Fprintf(res, "%+v", data)
 }
