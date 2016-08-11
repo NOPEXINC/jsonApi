ff9dd9de80a110ac6ef89dfe331ec5bd97241a4b
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-11 23:25:34 +0300

    Move GetJSON function to the api package

---
+++ api/api.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func GetJSON(url string, target interface{}) error {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	return json.NewDecoder(resp.Body).Decode(target)
+}
