e26dd36ce806327bdf055fa9b685bef4f865c330
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-11 23:48:18 +0300

    Add /prices endpoint and respective handler

--- main.go
+++ main.go
@@ -17,12 +17,25 @@ type Post struct {
 	Body   string `json:"body"`
 }
 
+type Food struct {
+	Price Price
+}
+
+type Price struct {
+	Fruits     Fruit
+	Vegetables Vegetable
+}
+
+type Fruit map[string]int
+type Vegetable map[string]int
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
 	}
 	http.HandleFunc("/api", PostsHandler)
+	http.HandleFunc("/prices", PriceHandler)
 	log.Println("listening on http://localhost:" + port)
 	http.ListenAndServe(":"+port, nil)
 }
@@ -37,4 +50,29 @@ func PostsHandler(res http.ResponseWriter, req *http.Request) {
 
 	log.Printf("accepting %s requests from http://localhost:4000%s", req.Method, req.URL)
 	fmt.Fprintf(res, "%+v", string(data))
+}
+
+func PriceHandler(res http.ResponseWriter, req *http.Request) {
+	fruits := make(map[string]int)
+	veggies := make(map[string]int)
+
+	fruits["Bananas"] = 1000
+	fruits["Mangoes"] = 200
+	fruits["Pineaples"] = 2400
+
+	veggies["Tomatoes"] = 200
+	veggies["Beans"] = 1500
+	veggies["Onions"] = 350
+
+	price := Price{fruits, veggies}
+	food := Food{price}
+
+	response, err := api.GetJsonResponse(&food)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("accepting %s requests from http://localhost:4000%s", req.Method, req.URL)
+	fmt.Fprintf(res, "%+v", string(response))
 }
